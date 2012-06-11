66881b5b8d30106b38d478e19eaead92c59eb2c1
Kyle Rohr <public@antipex.com>
2012-06-11 15:31:02 -0700

    Fixed an issue preventing multiple addtocal buttons from functioning on the same page.

--- addtocal.js
+++ addtocal.js
@@ -2,25 +2,24 @@
   Drupal.behaviors.addtocal = {
     attach: function(context) {
       // Handle toggle event to position the menu properly
-      jQuery('#addtocal').toggle(function(e) {
+      jQuery('.addtocal').toggle(function(e) {
         var offset = jQuery(this).position();
+        var button_id = jQuery(this).attr('id');
+        var $menu = jQuery('#' + button_id + '_menu');
         
-        jQuery('#addtocal_menu').css({
+        $menu.show();
+
+        $menu.css({
           'top': offset.top + 20,
           'left': offset.left
         });
         
         //Clicking outside the menu will hide it
         jQuery(document).bind('click', function() {
-          jQuery('#addtocal_menu').hide();
+          $menu.hide();
         });
       }, function() {
-        
-      });
-      
-      // Toggle the menu when the button is clicked
-      jQuery('#addtocal').click(function(e) {
-        jQuery('#addtocal_menu').toggle();
+        $menu.hide();
       });
     }
   } 
